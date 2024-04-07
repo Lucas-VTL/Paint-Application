9df8fbf0ed8f1438bee8eec16539b533158d579b
Lucas-VTL <votanloc1106@gmail.com>
2024-04-08 04:24:33 +0700

    Init UI for selection fuction

--- Paint Application/MainWindow.xaml.cs
+++ Paint Application/MainWindow.xaml.cs
@@ -35,9 +35,79 @@ namespace Paint_Application
 
     public partial class MainWindow : Window
     {
+        private bool isSelectionOpen = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            selectionCombobox.SelectedIndex = 0;
+        }
+
+        private void minimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void closeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void selectionButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!isSelectionOpen)
+            {
+                selectionCombobox.IsDropDownOpen = true;
+                selectionButtonContent.Source = new BitmapImage(new Uri("images/arrow-up.png", UriKind.Relative));
+                isSelectionOpen = true;
+            } else
+            {
+                selectionCombobox.IsDropDownOpen = false;
+                selectionButtonContent.Source = new BitmapImage(new Uri("images/arrow-down.png", UriKind.Relative));
+                isSelectionOpen = false;
+            }
+        }
+
+        private void selecionStackpanelMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            selectionBorder.Opacity = 1;
+        }
+
+        private void selectionComboboxPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            selectionButtonContent.Source = new BitmapImage(new Uri("images/arrow-down.png", UriKind.Relative));
+            isSelectionOpen = false;
+            selectionBorder.Opacity = 1;
+        }
+
+        private void selectionComboboxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = selectionCombobox.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    selectionImage.Source = new BitmapImage(new Uri("images/rec-selection.png", UriKind.Relative));
+                    break;
+                case 1:
+                    selectionImage.Source = new BitmapImage(new Uri("images/free-selection.png", UriKind.Relative));
+                    break;
+                case 2:
+                    selectionImage.Source = new BitmapImage(new Uri("images/all-selection.png", UriKind.Relative));
+                    break;
+                default: break;
+            }
+
+            selectionButtonContent.Source = new BitmapImage(new Uri("images/arrow-down.png", UriKind.Relative));
+            isSelectionOpen = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            selectionBorder.Opacity = 0;
         }
     }
 }
