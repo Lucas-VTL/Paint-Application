9f74f9e1838bd68f0cab52db6794365494e84f2c
Lucas-VTL <votanloc1106@gmail.com>
2024-04-20 21:49:17 +0700

    Complete drawing arrow (still get bugs on moving reverse left and top)

--- myArrow/myArrow.cs
+++ myArrow/myArrow.cs
@@ -3,6 +3,8 @@ using myShape;
 using myStroke;
 using myWidthness;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace myArrow
 {
@@ -38,7 +40,49 @@ namespace myArrow
         }
 
         public UIElement convertShapeType() {
-            return null;
+            Point start = startPoint;
+            Point end = endPoint;
+            Point center = new Point((startPoint.X + endPoint.X) / 2, (startPoint.Y + endPoint.Y) / 2);
+
+            var left = Math.Min(startPoint.X, endPoint.X);
+            var right = Math.Max(startPoint.X, endPoint.X);
+
+            var top = Math.Min(startPoint.Y, endPoint.Y);
+            var bottom = Math.Max(startPoint.Y, endPoint.Y);
+
+            var width = right - left;
+            var height = bottom - top;
+
+            var element = new Path
+            {
+                StrokeThickness = widthness.widthnessValue,
+                StrokeDashArray = strokeStyle.strokeValue,
+                Stroke = colorValue.colorValue,
+                Data = CreateArrowGeometry(center, start, end, width, height)
+            };
+
+            return element;
+        }
+
+        private Geometry CreateArrowGeometry(Point center, Point start, Point end, double width, double height)
+        {
+            var geometry = new PathGeometry();
+
+            var figure = new PathFigure
+            {
+                StartPoint = new Point(center.X, startPoint.Y),
+                IsClosed = true
+            };
+
+            figure.Segments.Add(new LineSegment(new Point(endPoint.X, center.Y - height / 6), true));
+            figure.Segments.Add(new LineSegment(new Point(center.X + width / 6, center.Y - height / 6), true));
+            figure.Segments.Add(new LineSegment(new Point(center.X + width / 6, endPoint.Y), true));
+            figure.Segments.Add(new LineSegment(new Point(center.X - width / 6, endPoint.Y), true));
+            figure.Segments.Add(new LineSegment(new Point(center.X - width / 6, center.Y - height / 6), true));
+            figure.Segments.Add(new LineSegment(new Point(startPoint.X, center.Y - height / 6), true));
+
+            geometry.Figures.Add(figure);
+            return geometry;
         }
     }
 }
